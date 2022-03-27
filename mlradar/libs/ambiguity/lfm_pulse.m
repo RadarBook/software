89f8004f567129216b92c156bbed658a9c03745a
Andy Harrison <pythonradarbook@gmail.com>
2022-03-27 12:15:36 -0500

    Update lfm_pulse.m
    Corrected LFM ambiguity function (removed pi from sinc function)

--- mlradar/libs/ambiguity/lfm_pulse.m
+++ mlradar/libs/ambiguity/lfm_pulse.m
@@ -13,7 +13,7 @@ function [ ambiguity ] = lfm_pulse(time_delay, doppler_frequency, pulse_width, b
 % This file is part of Introduction to Radar Using Python and MATLAB
 % and can not be copied and/or distributed without the express permission of Artech House.
 
-ambiguity = abs((1.0 - abs(time_delay) / pulse_width) .* sinc(pi * pulse_width .*...
+ambiguity = abs((1.0 - abs(time_delay) / pulse_width) .* sinc(pulse_width .*...
     (bandwidth / pulse_width * time_delay + doppler_frequency) .* (1.0 - abs(time_delay) / pulse_width))).^2;
 
 if length(time_delay) == 1 && abs(time_delay) > pulse_width
